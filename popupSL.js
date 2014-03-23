c638f7169ac82ec47a2431eeb75a671862a03851
raddar4 <raddar360@gmail.com>
2014-03-23 03:51:14 -0400

    SLv2

--- popupSL.js
+++ popupSL.js
@@ -37,17 +37,8 @@ function getSelectionHtml() {
 function handleKeypress(){
 	if (event.keyCode == 84){
 		var words = getSelectionHtml().split(" ");
-		//var newWords = new Array();
 		for (var i =0; i < words.length; ++i) {
 				alert(synonym(words[i]));
 			}
-		}
-		/*
-		var newSentence = "";
-		for (var i = 0; i < newWords.length; ++i) {
-			newSentence = newSentence + newWords[i] + " ";
-		}
-		alert(newSentence);
-		*/
-	}
+		}	
 }
