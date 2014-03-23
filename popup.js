7dcdea021f483680544f2c52f85c39768912ea8a
Andy Le <andyle2010@gmail.com>
2014-03-23 04:12:12 -0400

    who knows what this is

--- popup.js
+++ popup.js
@@ -11,24 +11,29 @@ function handleKeypress(){
 		var words = html.split(" ");
 		console.log(words);
 		var newWords = new Array();
-		
 		//round 2
-		var temp' 
+		var temp; 
+		alert(".5");
 		for (var i =0; i < words.length; ++i) {
+			
 			if (words[i] != ""){
+				alert("1");
 				var site = "http://api.wolframalpha.com/v2/query?appid=J5UPVW-4RLV6H2X3E&input=synonym%20" + words[i] + "&format=plaintext&includepodid=Synonyms:WordData";
-				$.get(
-					site,
-					function(data) {
+				$.get(site, function(data) {alert("2");
+
 						var text = new XMLSerializer().serializeToString(data);
 						var syns = text.match(/\<plaintext>.*\|/) + ">";
 						synArray = syns.split(">");
 						synArray = synArray[1].split(" ");
+						temp = (synArray[0]);
+						alert(synArray[0] + "fe");
 						newWords[i] =(synArray[0]);
 					}
 				);
+										alert(temp);
+
 				html = "";
-			}
+			} 
 		}
 		var newSentence = "";
 		for (var i = 0; i < newWords.length; ++i) {
