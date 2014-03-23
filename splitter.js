16d64701b3d69eac9961cebf0ff00c578ac67803
Andy Le <andyle2010@gmail.com>
2014-03-22 23:33:21 -0400

    updated splitter; still doesn't work

--- splitter.js
+++ splitter.js
@@ -1,6 +1,29 @@
-var selected = "this is what is selected right now. THis is only for testing purposes."
+var selected = "this is what is selected right now. THis is only for testing purposes.";
+document.write("<h1>This is a heading</h1>");
 
 var words = selected.split(" ");
 console.log(words);
 
-alert(words);
+var temp = "large";
+$.get("http://words.bighugelabs.com/api/2/e2b80f9b79a99670c738434e668ebc08/" + temp + "/", 
+		function (data) {
+		document.write(data);
+	}
+	);
+	
+	
+/*
+for (var i = 0; i < words.length - 1; ++i) {
+	//alert(html);
+	var site = "http://words.bighugelabs.com/api/2/e2b80f9b79a99670c738434e668ebc08/" + words[i] + "/";
+			$.get(
+				site,function(data) {
+					alert(data);
+				}
+			);
+		html = "";
+	}
+alert(selected);*/
+document.write("<h1>This is a closing</h1>");
+
+
