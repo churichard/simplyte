5a3b379a4a1669e83e556a9a890095c115433305
Andy Le <andyle2010@gmail.com>
2014-03-22 23:44:01 -0400

    splitter outputs words seperately in the array

--- splitter.js
+++ splitter.js
@@ -4,9 +4,14 @@ document.write("<h1>This is a heading</h1>");
 var words = selected.split(" ");
 console.log(words);
 
+for (var i= 0; i < words.length -1; ++i) {
+	document.write(words[i] + "<br>");
+	}
+
 var temp = "large";
 $.get("http://words.bighugelabs.com/api/2/e2b80f9b79a99670c738434e668ebc08/" + temp + "/", 
 		function (data) {
+		alert(data);
 		document.write(data);
 	}
 	);
